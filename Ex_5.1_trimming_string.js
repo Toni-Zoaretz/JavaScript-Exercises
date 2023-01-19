a76c7c69d23abc5976e9b66432032aea93e9ed34
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-20 00:11:21 +0200

    5.3

---
+++ Ex_5.1_trimming_string.js
@@ -0,0 +1,5 @@
+// function removeFirstAndLast(str) {
+//   return str.slice(1, str.length - 1);
+// }
+
+// console.log(removeFirstAndLast("mariontea"));
---
+++ Ex_5.2_String_Repeat.js
@@ -0,0 +1,5 @@
+// function repeat_str(number, str) {
+//   return str.repeat(number);
+// }
+
+// console.log(repeat_str(5, "Hello"));
