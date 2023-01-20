a0e0b70fe1dff2e1180e34803145afe0b619b6b5
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-20 10:37:37 +0200

    5.3

--- Ex_5.3_To_Camel_Case.js
+++ Ex_5.3_To_Camel_Case.js
@@ -1,3 +1,31 @@
 // Complete the method/function so that it converts dash/underscore delimited words into camel
 // casing. The first word within the output should be capitalized only if the original word was
 // capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+// Examples
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior
+
+function convertDashCamel(str) {
+  let result = "";
+  const arr = str.split('');
+  for(let i = 0; i < arr.length; i++)
+}
+
+console.log(convertDashCamel("the-stealth-warrior"));
+
+// function isLetter(str) {
+//   return str.length === 1 && str.match(/[a-z]/i);
+// }
+// let test = "the-stealth-warrior";
+// console.log(isLetter("a"));
+// const split1 = test.split("");
+// console.log(split1.length);
+
+function ax(str) {
+  if ((str >= "A" && str <= "Z") || (str >= "a" && str <= "z")) {
+    return true;
+  }
+  return false;
+}
+
+console.log(ax("/"));
