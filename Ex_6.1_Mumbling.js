99176a6f48fb2074a68a60da6202332c8f35ddb3
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-21 12:24:49 +0200

    6.1

--- Ex_6.1_Mumbling.js
+++ Ex_6.1_Mumbling.js
@@ -1,13 +1,11 @@
-// ("abcd") => "A-Bbb-Ccc-Dddd";
+// function accum(str) {
+//   let result1 = "";
+//   let result2 = "";
+//   for (let i = 1; i < str.length; i++) {
+//     result1 += "-" + str.charAt(i).toUpperCase() + str.charAt(i).repeat(i);
+//   }
 
-function accum(str) {
-  let result1 = "";
-  let result2 = "";
-  for (let i = 1; i < str.length; i++) {
-    result1 += "-" + str.charAt(i).repeat(i + 1);
-  }
+//   return str.charAt(0).toUpperCase() + result1;
+// }
 
-  return result1;
-}
-
-console.log(accum("abcd")); //1*2*3*4*5 == 120
+// console.log(accum("abcd"));
